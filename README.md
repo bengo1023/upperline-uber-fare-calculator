c630bb79c1571c3ffcc6dbaa9a7cc6c9b1723960
richardpremvaree <richard.premvaree@gmail.com>
2016-08-09 03:40:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Additional capabilities:
+
+How does surge pricing affect your calculator?
+Do all ubers have the same surge pricing multiplier?
+Can your calculator print out which uber is the best option?
+How can your calculator tell you whether to take an uber or a yellow taxi?
---
+++ uber-fare-calculator.rb
@@ -0,0 +1,12 @@
+## DEFINE YOUR METHODS HERE
+#write your method for uberX here. It should have an argument for distance and for time.
+def u
+#write your method for uberBlack here
+
+#write your method for uberXL here
+
+#write your method for uberSUV here
+
+
+##  CALL/USE YOUR METHODS HERE
+
