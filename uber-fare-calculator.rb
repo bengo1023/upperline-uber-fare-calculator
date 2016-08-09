9716fd6fe7c96d09c559dfaf74a2dffcb3c6c893
richardpremvaree <richard.premvaree@gmail.com>
2016-08-09 03:59:22 +0000

    revised readme

--- uber-fare-calculator.rb
+++ uber-fare-calculator.rb
@@ -1,6 +1,7 @@
 ## DEFINE YOUR METHODS HERE
-#write your method for uberX here. It should have an argument for distance and for time.
-def u
+#write your method for uberX here
+def uberX_fare_calculator
+end
 #write your method for uberBlack here
 
 #write your method for uberXL here
@@ -9,4 +10,3 @@ def u
 
 
 ##  CALL/USE YOUR METHODS HERE
-
